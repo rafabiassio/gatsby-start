60269f478211bf7ffde8ed2ed01d6c9f6f0b892a
Rafael Biassio <rafael.biassio@e-deploy.com.br>
2020-02-05 17:47:43 -0400

    Initial blog post page with GraphQL

--- my-blog/gatsby-node.js
+++ my-blog/gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+// To add slug fiel to each post
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: "pages",
+    })
+
+    // Creates new query'able field with name of 'slug'
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/${slug.slice(12)}`,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+
+    return graphql(`
+    {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }        
+    `)
+    .then(result => {
+        const list = result.data.allMarkdownRemark.edges
+
+        for(let i = 0; i < list.length; i++) {
+            const { node } = list[i]
+
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve('./src/templates/blog-post.js'),
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        }
+    })
+}
--- my-blog/src/pages/index.js
+++ my-blog/src/pages/index.js
@@ -1,21 +1,60 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem 
-      slug="/about/"
-      category="code" 
-      date="05 de fevereiro de 2020"
-      timeToRead="5"
-      title="Diga não ao Java"
-      description="Algumas razões para você não usar Java."
-    />
-  </Layout>
-)
+const IndexPage = () => {
+
+  const { allMarkdownRemark } = useStaticQuery(graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }    
+    `
+    )
+
+    const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(({ 
+        node: {
+          fields: { slug },
+          frontmatter: { background, category, date, description, title },
+          timeToRead
+        }
+       }) => (
+        <PostItem 
+          slug={slug}
+          background={background}
+          category={category}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+        />
+       ))}
+    </Layout>
+  )
+  
+}
 
 export default IndexPage
